f1daefbbbbf6641e54a8df7afa7b08e50e525722
Aski Bilgi İşlem Daire Başkanlğı <mustafa.turkyasar@gmail.com>
2020-07-07 08:32:42 +0300

    present js

--- pages/_app.js
+++ pages/_app.js
@@ -4,33 +4,9 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <title>Pet Care App</title>
-      </Head>
+    return <Component {...pageProps} />
 
-      <div className="top-bar">
-        <div className="nav">
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-          <Link href="/new">
-            <a>Add Pet</a>
-          </Link>
-        </div>
 
-        <img
-          id="title"
-          src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Pet_logo_with_flowers.png"
-          alt="pet care logo"
-        ></img>
-      </div>
-      <div className="grid wrapper">
-        <Component {...pageProps} />
-      </div>
-    </>
-  )
 }
 
 export default MyApp
--- pages/present.js
+++ pages/present.js
@@ -1,12 +1,131 @@
-
 import Layout from "../components/Layout";
+import Header from "../components/Header";
 
 
+import fetch from "isomorphic-unfetch";
 
-export default function HomePage() {
+import Head from "next/head";
+export default function present(query) {
     return (
         <Layout>
-           <h1>oldu reis bey oldu</h1>
+            <Head>
+                <script src="/js/custom.js"></script>
+
+            </Head>
+            <div className="super_container">
+                {console.log(query)}
+                <Header name={query.name} id={query.id}></Header>
+
+                <div className="home">
+                    <div className="hero_slider_container">
+                        <div className="hero_slider owl-carousel">
+
+                            <div className="hero_slide">
+                                <div
+                                    className="hero_slide_background"
+                                    style={{
+                                        backgroundImage:
+                                            "url(" +
+                                            "https://images.pexels.com/photos/34153/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=350" +
+                                            ")",
+                                        backgroundPosition: "center",
+                                        backgroundSize: "cover",
+                                        backgroundRepeat: "no-repeat",
+                                    }}
+                                />
+                                <div className="hero_slide_container d-flex flex-column align-items-center justify-content-center">
+                                    <div className="hero_slide_content text-center">
+                                        <h1
+                                            data-animation-in="fadeInUp"
+                                            data-animation-out="animate-out fadeOut"
+                                        >
+                                            Uzaktan Özel <span>Matematik</span> dersleri!
+                                        </h1>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="hero_slide">
+                                <div
+                                    className="hero_slide_background"
+                                    style={{
+                                        background: "url('/images/slider_background.jpg')",
+                                    }}
+                                />
+                                <div className="hero_slide_container d-flex flex-column align-items-center justify-content-center">
+                                    <div className="hero_slide_content text-center">
+                                        <h1
+                                            data-animation-in="fadeInUp"
+                                            data-animation-out="animate-out fadeOut"
+                                        >
+                                            Öğrenci Koçluğu ve <span>Matematik</span> Özel Ders!
+                                        </h1>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="hero_slider_left hero_slider_nav trans_200">
+                            <span className="trans_200">geri</span>
+                        </div>
+                        <div className="hero_slider_right hero_slider_nav trans_200">
+                            <span className="trans_200">ileri</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="hero_boxes">
+                    <div className="hero_boxes_inner">
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-lg-4 hero_box_col">
+                                    <div className="hero_box d-flex flex-row align-items-center justify-content-start">
+                                        <img src="/images/earth-globe.svg" className="svg" alt="" />
+                                        <div className="hero_box_content">
+                                            <h2 className="hero_box_title">Matematik Özel Ders</h2>
+                                            <a href="courses.html" className="hero_box_link">
+                                                Detaylar
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 hero_box_col">
+                                    <div className="hero_box d-flex flex-row align-items-center justify-content-start">
+                                        <img src="images/books.svg" className="svg" alt="" />
+                                        <div className="hero_box_content">
+                                            <h2 className="hero_box_title">
+                                                Uzaktan Canlı Matemetik Özel Ders
+                                            </h2>
+                                            <a href="courses.html" className="hero_box_link">
+                                                Detaylar
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div className="col-lg-4 hero_box_col">
+                                    <div className="hero_box d-flex flex-row align-items-center justify-content-start">
+                                        <img src="images/professor.svg" className="svg" alt="" />
+                                        <div className="hero_box_content">
+                                            <h2 className="hero_box_title">Koçluk Sistemi</h2>
+                                            <a href="teachers.html" className="hero_box_link">
+                                                Detaylar
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     );
 }
+
+present.getInitialProps = async ({ req, query }) => {
+    // const res = await fetch('/api/files')
+
+    return { stars: 'json', user: 'json2' ,query};
+};
